b0e8953b7ce0f50bdc8ccd05cfd91c8fd25b675b
unknown <gbemiemmanuel555@gmail.com>
2024-04-25 15:26:32 +0100

    job page added

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,25 @@
 // import { useState, useEffect } from 'react';
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const job = useLoaderData();
+
+    
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete theis listing?');
+
+        if (!confirm) return;
+
+        deleteJob(jobId);
+
+        navigate('/jobs');
+    }
+
     // const [job , setJob] = useState(null);
     // const [loading, setLoading] = useState(true);
     
@@ -30,7 +43,7 @@ const JobPage = () => {
     <section>
       <div className="container m-auto py-6 px-6">
         <Link
-          to="/"
+          to="/jobs"
           className="text-indigo-500 hover:text-indigo-600 flex items-center"
         >
           <FaArrowLeft className='mr-2'/> Back to Job Listings
@@ -103,7 +116,7 @@ const JobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
-              <button
+              <button onClick={ () => onDeleteClick(job.id) }
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
